dbca6193d0d350997eda4c9688139424789c84d0
Brenda Ayala <brenda.ayala@ciencias.unam.mx>
2022-09-18 22:05:46 -0500

    Update: Request

--- src/Clima/datos/Request.py
+++ src/Clima/datos/Request.py
@@ -1,9 +1,15 @@
 import requests
+from dotenv import load_dotenv
 from functools import lru_cache
+import os
+
+def config():
+    load_dotenv()
 
 @lru_cache(maxsize=None)
 
 def clima (lat, lon):
-   url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=df3f287580fda7c0dab43d1051a30497'
+   config()
+   url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={os.getenv("api")}'
    data = requests.get(url).json()
    return data['main']
