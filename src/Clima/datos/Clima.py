d0175d628420bc235538603c917edcc84f061526
Brenda Ayala <brenda.ayala@ciencias.unam.mx>
2022-10-04 21:04:22 -0500

    mejora de Api_Key

--- src/Clima/datos/Clima.py
+++ src/Clima/datos/Clima.py
@@ -1,11 +1,11 @@
 from Datos import coordenadas
-from Request import clima
+from Request import peticion
 
 #Pide las coordenadas de los estados de origen y destino
 def Peticion(orig, destino):
     cor_orig = coordenadas(orig, True)
     cor_dest = coordenadas(destino, False)
-    clima_orig = clima(cor_orig[0], cor_orig[1])
-    clima_dest = clima(cor_dest[0], cor_dest[1])
+    clima_orig = peticion(cor_orig[0], cor_orig[1])
+    clima_dest = peticion(cor_dest[0], cor_dest[1])
     return clima_orig, clima_dest
 
