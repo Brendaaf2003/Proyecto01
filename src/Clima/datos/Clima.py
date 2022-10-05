05d48d45404b8af5a6c4f40f0a3635945594952b
Brenda Ayala <brenda.ayala@ciencias.unam.mx>
2022-10-04 22:34:21 -0500

    Pulcritud

--- src/Clima/datos/Clima.py
+++ src/Clima/datos/Clima.py
@@ -1,11 +1,12 @@
-from Datos import coordenadas
-from Request import peticion
+from Datos import extraer_coordenadas
+from Request import peticion_lat_lon
 
 #Pide las coordenadas de los estados de origen y destino
-def Peticion(orig, destino):
-    cor_orig = coordenadas(orig, True)
-    cor_dest = coordenadas(destino, False)
-    clima_orig = peticion(cor_orig[0], cor_orig[1])
-    clima_dest = peticion(cor_dest[0], cor_dest[1])
+def pedir_peticion(orig, destino):
+    '''Esta función pide las coordenadas de los estados de origen y destino'''
+    cor_orig = extraer_coordenadas(orig, True)
+    cor_dest = extraer_coordenadas(destino, False)
+    clima_orig = peticion_lat_lon(cor_orig[0], cor_orig[1])
+    clima_dest = peticion_lat_lon(cor_dest[0], cor_dest[1])
     return clima_orig, clima_dest
 
--- src/Clima/datos/Datos.py
+++ src/Clima/datos/Datos.py
@@ -2,24 +2,24 @@ from requests import *
 import pandas as pd
 import os
 
-rutaAbsoluta1 = open('Ruta_Absoluta.txt', 'r')
+rutaAbsoluta1 = open('Ruta_Absoluta.txt', 'r').read()
 
 
 #Esta función lee la base de datos y la convierte en un dataframe
-def Base_de_datos(rutaAbsoluta = rutaAbsoluta1):
+def leer_base_de_datos(rutaAbsoluta = rutaAbsoluta1):
     df = pd.read_csv(rutaAbsoluta)
     return df.drop_duplicates()
 
 #Esta función filtra los vuelos unicos
-def vuelos_unicos(rutaAbsoluta = rutaAbsoluta1):
-    df = Base_de_datos(rutaAbsoluta)
+def obtener_vuelos_unicos(rutaAbsoluta = rutaAbsoluta1):
+    df = leer_base_de_datos(rutaAbsoluta)
     df = df[['origin', 'destination']].drop_duplicates()
     df['ID_Vuelo'] = [i for i in range(1, len(df) + 1)]
     return df
 
 #Esta función extrae las coordenadas de los estados de origen y destino
-def coordenadas(edo, orig = True, rutaAbsoluta = rutaAbsoluta1):
-    df = Base_de_datos(rutaAbsoluta)
+def extraer_coordenadas(edo, orig = True, rutaAbsoluta = rutaAbsoluta1):
+    df = leer_base_de_datos(rutaAbsoluta)
     if orig:
         return df[df['origin'] == edo][['origin_latitude', 'origin_longitude']].values[0]
     else:   
